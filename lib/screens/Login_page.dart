081b399b84546259ce6c7b2929287502e55b917a
UlbertAllain <igrii.noel@gmail.com>
2024-07-02 21:03:17 +0700

    revisi

--- lib/screens/Login_page.dart
+++ lib/screens/Login_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:uts_project/screens/HomePage.dart';
-import 'package:uts_project/Register_Page.dart';
 import 'package:uts_project/screens/ProductListScreen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
